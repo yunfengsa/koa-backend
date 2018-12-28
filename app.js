a81658ecf570f3f89e7907888d09eb0da5549e5a
律勤 <jialong.ljl@alibaba-inc.com>
2018-12-28 17:51:45 +0800

    fix

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const Koa = require('koa');
+const body = require('koa-body');
 const router = require('./router/index');
 const jsonp = require('./middleware/koa-jsonp');
 
 const {getFlavorCope} = require('./services/puppeteer');
 
 const app = new Koa();
+app.use(body({
+  multipart: true
+}))
 app.use(jsonp());
 app.use(router.routes(), router.allowedMethods());
 
